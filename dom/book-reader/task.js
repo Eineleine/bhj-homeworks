da05c1d5e3f5308c462444dd1f8a488b92b1628f
Eineleine <dsa210487@mail.ru>
2024-07-15 23:42:42 +0300

    Book-reader task is fixed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -9,14 +9,16 @@ for (let i = 0; i < fontSize.length; i++) {
         }
         fontSize[i].classList.toggle('font-size_active');
         if (fontSize[i].dataset.size === 'small') {
-            content.classList.toggle('book_fs-small');
+            content.classList.add('book_fs-small');
+            content.classList.remove('book_fs-big');
         }
         if (fontSize[i].dataset.size === 'big') {
-            content.classList.toggle('book_fs-big');
+            content.classList.add('book_fs-big');
+            content.classList.remove('book_fs-small');
         }
         if (!fontSize[i].dataset.size) {
-            content.classList.toggle('book_fs-big', false);
-            content.classList.toggle('book_fs-small', false);
+            content.classList.remove('book_fs-big');
+            content.classList.remove('book_fs-small');
         }
     })
 }
