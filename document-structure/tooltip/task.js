f4146b193decd6e5a6dd42c16fd636e0117b8889
Eineleine <dsa210487@mail.ru>
2024-08-12 18:31:05 +0300

    Tooltip task is ready

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,17 +3,25 @@ const links = [...document.querySelectorAll('.has-tooltip')];
 links.forEach(link => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
-        const toolTip = link.querySelector('.tooltip');
+        //console.log(link);
+        const pos = link.getBoundingClientRect();
+        //console.log(link.textContent);
+        console.log(pos);
+        const toolTip = document.querySelector('.tooltip');
         if (toolTip) {
             toolTip.classList.toggle('tooltip_active');
         } else {
-            const newToolTip = document.createElement('div');
+            
+            link.insertAdjacentHTML('afterEnd',   
+                `<div class="tooltip tooltip_active" style="left: ${pos.left}, top: (${pos.y} + ${pos.height})">
+                    ${link.title}
+                </div>`)
+           /* const newToolTip = document.createElement('div');
             newToolTip.className = 'tooltip';
             newToolTip.classList.add('tooltip_active');
             newToolTip.style = 'left: 0; top: 0';
             newToolTip.innerText = link.title;
-            link.append(newToolTip);
+            div.append(newToolTip);*/
         }
     });
 });
-
